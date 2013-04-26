d3a0adaacb2537ab23f3244197aa7abe0ba879d0
Anna Cruz <anna.cruz@gmail.com>
2013-04-26 06:57:14 -0300

    Solving same logic issue

--- french_multiply.cpp
+++ french_multiply.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int multiply(int num1, int num2){
+
+  int temp_result = num1 * (num2 / 2);
+
+  if (num2 == 0)
+  {
+   return 0;
+  }
+
+  if (num2 % 2 == 0)
+  {
+    return 2 * temp_result;
+  } else
+  {
+    return num1 + (2 * temp_result);
+  }
+
+}
+
 int main()
+
 {
+
   int num1, num2;
   cout << "Please enter the first number: ";
   cin >> num1;
   cout << "Please enter the second number: ";
   cin >> num2;
 
-  int temp_result = num1 * (num2 / 2);
-
-
-  if (num2 == 0)
-  {
-    cout << 0;
-  }
-
-  if (num2 % 2 == 0)
-  {
-    cout << 2 * temp_result;
-  } else
-  {
-    cout << num1 + (2 * temp_result);
-  }
-
+  int result = multiply(num1, num2);
+  cout << result;
 
 }
